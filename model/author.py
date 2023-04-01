866a231b9d18e4bc08ba66fddecb1a22661950fc
Kontact <duyan5745@gmail.com>
2023-04-01 19:18:23 +0700

    Update Model, add dbquery.py for making mongodb query

--- model/author.py
+++ model/author.py
@@ -1,40 +1,38 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from article import Article
-
-
 class Author:
-    def __init__(self, username: str, password, real_name: str):
+    def __init__(self, username: str, password, real_name: str, ID: int, email="", bio="", expertise="",
+                 publication_history=None):
+        if publication_history is None:
+            publication_history = []
         self.__username = username
         self.__password = password
         self.__name = real_name
-        self.__email = ""
-        self.__bio = ""
-        self.__expertise = ""
-        self.__publication_history: list[Article] = []
+        self.__id = ID
+        self.__email = email
+        self.__bio = bio
+        self.__expertise = expertise
+        self.__publication_history: list[int] = publication_history
 
     # Getters
+    def get_id(self) -> int: return self.__id
     def get_username(self) -> str: return self.__username
     def get_password(self) -> str: return self.__password   # Return the hashed password for comparison
-    def get_name(self) -> str: return self.__display_name
+    def get_name(self) -> str: return self.__name
     def get_email(self) -> str: return self.__email
     def get_bio(self) -> str: return self.__bio
     def get_expertise(self) -> str: return self.__expertise
-    def get_publication_history(self) -> list[Article]: return self.__publication_history
+    def get_publication_history(self) -> list[int]: return self.__publication_history
 
     # Setters
     def set_password(self, password): self.__password = password    # Incase change password
-    def set_name(self, name): self.__display_name = name    # Incase change display name
+    def set_name(self, name): self.__name = name    # Incase change real name
     def set_email(self, email): self.__email = email
     def set_bio(self, bio): self.__bio = bio
     def set_expertise(self, expertise): self.__expertise = expertise
 
-    def add_article(self, article: Article):
+    def add_article(self, article_id: int):
         """
         Add an article to the publication history of the author
-        :param article: Article object
+        :param article_id: ID of the article
         :return: None
         """
-        self.__publication_history.append(article)
+        self.__publication_history.append(article_id)
---
+++ model/dbquery.py
@@ -0,0 +1,133 @@
+from pymongo import MongoClient
+from .article import Article
+from .author import Author
+
+
+class Database:
+    def __init__(self):
+        self.__client = MongoClient('mongodb://localhost:27017/')
+        self.__database = self.__client['newspapers_db']
+        self.articles_collection = self.__database['newspapers']
+        self.authors_collection = self.__database['authors']
+
+    def count_all_authors(self) -> int:
+        return self.articles_collection.count_documents({})
+
+    def count_all_articles(self) -> int:
+        return self.articles_collection.count_documents({})
+
+    def get_all_articles(self) -> list[Article]:
+        article_objs: list[Article] = []
+        articles = self.articles_collection.find()
+        for article in articles:
+            date = article['date']
+            category = article['categories']
+            viewed = article['viewed']
+            title = article['title']
+            description = article['description']
+            author = article['author']
+            content = article['content']
+            ID = article['_id']
+            new_article = Article(date, category, viewed, title, description, author, content, ID)
+            article_objs.append(new_article)
+        return article_objs
+
+    def add_articles_to_db(self, article: Article):
+        articles_data = {
+            '_id': article.get_id(),
+            'date': article.get_date(),
+            'category': article.get_categories(),
+            'viewed': article.get_views(),
+            'title': article.get_title(),
+            'description': article.get_overview(),
+            'author': article.get_author(),
+            'content': article.get_content(),
+        }
+
+        # Insert the article into the database
+        self.articles_collection.insert_one(articles_data)
+
+    def add_author_to_db(self, author: Author):
+        authors_data = {
+            '_id': author.get_id(),
+            'username': author.get_username(),
+            'password': author.get_password(),
+            'name': author.get_name(),
+            'email': author.get_email(),
+            'bio': author.get_bio(),
+            'expertise': author.get_expertise(),
+            'publication_history': author.get_publication_history()
+        }
+
+        # Insert the author into the database
+        self.authors_collection.insert_one(authors_data)
+
+    def check_exist_username(self, username: str) -> bool:
+        """
+        :return: True when the username is in the database, False otherwise
+        """
+        result = self.authors_collection.find({'username': username})
+        if result is None:
+            return False
+        return True
+
+    def delete_article(self, ft: dict):
+        self.articles_collection.delete_one(ft)
+
+    def get_all_categories(self) -> list[str]:
+        categories = self.articles_collection.distinct('categories')
+        return categories
+
+    def get_article_by_title(self, text: str):
+        articles = []     # list of article
+        results = self.articles_collection.find()
+        for result in results:
+            if text in result['title']:
+                articles.append(result)
+        return articles
+
+    def get_article_by_category(self, category: str):
+        articles = []
+        result = self.articles_collection.find({'categories': category})
+        for article in result:
+            articles.append(article)
+        return articles
+
+    def get_article_by_id(self, ID):
+        return self.articles_collection.find_one({'_id': ID})
+
+    def get_articles_sort_views(self, limit: int):
+        articles = []
+        result = self.articles_collection.find().sort('viewed', -1).limit(limit)
+        for article in result:
+            articles.append(article)
+        return articles
+
+    def get_articles_sort_date(self, limit: int):
+        articles = []
+        result = self.articles_collection.find().sort('date', -1).limit(limit)
+        for article in result:
+            articles.append(article)
+        return articles
+
+    def get_author_by_name(self, name: str):
+        author = self.authors_collection.find_one({'name': name})
+        return author
+
+    def update_pub_history(self, author_id: int, article_id: int):
+        # Get the author's publication history
+        history = self.authors_collection.find_one({'_id': author_id}, {'_id': 0, 'publication_history': 1})
+        history_array = history['publication_history']
+        # Add the new article to the history
+        history_array.append(article_id)
+        # Update the author's publication history
+        self.authors_collection.update_one({'_id': author_id}, {'$set': {'publication_history': history_array}})
+
+    def set_email(self, author_id: int, email: str):
+        self.authors_collection.update_one({'_id': author_id}, {'$set': {'email': email}})
+
+    def set_bio(self, author_id: int, bio: str):
+        self.authors_collection.update_one({'_id': author_id}, {'$set': {'bio': bio}})
+
+    def set_expertise(self, author_id: int, expertise: str):
+        self.authors_collection.update_one({'_id': author_id}, {'$set': {'expertise': expertise}})
