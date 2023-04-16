84a2f6c994862cc360fc7cf07189f042d5ee9467
Hoang Bui <canhchimcuajin@gmail.com>
2023-04-16 13:47:32 +0700

    Oops test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://static.wikia.nocookie.net/oshi_no_ko/images/b/b0/AiHoshino.png/revision/latest?cb=20210820021211"
-
-# request then save the image
-r = requests.get(url, allow_redirects=True)
-open('test.png', 'wb').write(r.content)
