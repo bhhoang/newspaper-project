289924d69b981f22f0f270be83cab86e19efd8b7
Hoang Bui <canhchimcuajin@gmail.com>
2023-04-17 01:17:29 +0700

    Fix gitignore

--- test.py
+++
@@ -1,6 +0,0 @@
-import bcrypt
-
-password = "admin"
-hashed = "$2b$12$rM0Tj8g5vEb8ITOLLV6Rq.LWeeitvI.JTy3aNMb/wsDFPm10RzIAO"
-
-print(bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8')))
